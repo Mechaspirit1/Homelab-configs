bd1174dfac02919e774558e16227d4972c7fdd63
mspirit1 <pabloloschi200@gmail.com>
2026-05-10 18:57:52 -0300

    Update to current configuration

--- dnt.sh
+++ dnt.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env bash
 
-#echo "OMNISSIAH ONLINE | VMs: $(qm list | grep running | wc -l) running | Uptime: $(uptime -p) | Date and time: $(date +'%d/%m/%Y %H:%M:%S')"
+#echo "OMNISSIAH ONLINE | VMs: $(qm list | grep running | wc -l) running | LXCs: $(pct list | grep running | wc -l) running | Uptime: $(uptime -p) | Date and time: $(date +'%d/%m/%Y %H:%M:%S')"
 while true; do
-    printf "\rOMNISSIAH ONLINE | VMs: %s running | Uptime: %s | Date and time: %s" \
+    printf "\rOMNISSIAH ONLINE | VMs: %s running | LXCs: %s running | Uptime: %s | Date and time: %s" \
         "$(qm list | grep running | wc -l)" \
+	"$(pct list | grep running | wc -l)" \
         "$(uptime -p)" \
         "$(date +'%d/%m/%Y %H:%M:%S')"
     sleep 0.1
