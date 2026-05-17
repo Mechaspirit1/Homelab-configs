bcfc70971ebc59b40101b4b713cf7cc49085dad6
mspirit1 <pabloloschi200@gmail.com>
2026-05-17 16:02:00 -0300

    Minor change to readme_consid

--- dnt.sh
+++ dnt.sh
@@ -2,10 +2,10 @@
 
 #echo "OMNISSIAH ONLINE | VMs: $(qm list | grep running | wc -l) running | LXCs: $(pct list | grep running | wc -l) running | Uptime: $(uptime -p) | Date and time: $(date +'%d/%m/%Y %H:%M:%S')"
 while true; do
-    printf "\rOMNISSIAH ONLINE | VMs: %s running | LXCs: %s running | Uptime: %s | Date and time: %s" \
+    printf "\rOMNISSIAH ONLINE| VMs: %s running | LXCs: %s running | Uptime: %s |Date and time: %s" \
         "$(qm list | grep running | wc -l)" \
 	"$(pct list | grep running | wc -l)" \
         "$(uptime -p)" \
         "$(date +'%d/%m/%Y %H:%M:%S')"
-    sleep 0.1
+    sleep 1
 done
